5262c4beca20c974979e17e452228908a28c0163
Tapu106 <tapusust993@gmail.com>
2021-10-31 16:36:53 +0600

    get character by id operation added

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,17 @@
-const { getCharacters } = require("../dynamo");
+const { getCharacters, getCharacterById } = require("../dynamo");
 exports.getAllCharactersInfo = async (req, res, next) => {
   const allCharactersInfo = await getCharacters();
-  console.log(allCharactersInfo);
-  return res.json({ allCharactersInfo });
+  // console.log(allCharactersInfo);
+  return res.json({
+    allCharactersInfo,
+  });
+};
+
+exports.getCharacterByID = async (req, res, next) => {
+  const { id } = req.params;
+  const characterInfo = await getCharacterById(id);
+  // console.log(characterInfo);
+  return res.json({
+    characterInfo,
+  });
 };
--- routes/home.js
+++ routes/home.js
@@ -4,4 +4,6 @@ const homeController = require("../controllers/home");
 
 router.get("/", homeController.getAllCharactersInfo);
 
+router.get("/characters/:id", homeController.getCharacterByID);
+
 module.exports = router;
